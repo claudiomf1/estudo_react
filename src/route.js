2eabe503da0b4fcd8aee647f12c30d4e112fa86d
claudiomf1 <claudiomf1+1@gmail.com>
2023-01-03 16:23:41 -0300

    componente search funcionando em localhost e em producao

--- src/route.js
+++ src/route.js
@@ -1,18 +1,13 @@
-
-import React from 'react'
-import {  Routes, Route } from 'react-router-dom'
-import Home from './components/pages/Home'
-
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import Home from "./components/pages/Home"
+// import "bootstrap/dist/css/bootstrap.min.css"
 
 export default function Routes() {
-  return (
-
-    <Routes>
-      
-          <Route  path="*" element={<Home />}/>
-          <Route  path="/home" element={<Home />}/>
-          
-  </Routes>
-
-  )
+   return (
+      <Routes>
+         <Route path="*" element={<Home />} />
+         <Route path="/home" element={<Home />} />
+      </Routes>
+   )
 }
