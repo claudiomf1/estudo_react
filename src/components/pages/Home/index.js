b38aa238649b2ae25abb8472ab94f4446d36e026
claudiomf1 <claudiomf1+1@gmail.com>
2023-01-03 16:23:41 -0300

    componente search funcionando em localhost e em producao

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,16 +1,37 @@
-import React from 'react';
-
+import React from "react"
+import SelectSourch from "../../SelectSearch/SelectSearch"
+import useGoogleSheets from "use-google-sheets"
 // import { Container } from './styles';
 
 function Home() {
-  return(
-    <>
+   const {
+      data: response,
+      loading,
+      error,
+   } = useGoogleSheets({
+      apiKey: "AIzaSyDs11gmvAQb2xdRL_fWVvhrTRKyz4NoZ5w",
+      sheetId: "1DHcDn2eTzk6VNa3x3fhQ1X_RgTpGHfV09VWxHD2gk54",
+      sheetsOptions: [{ id: "Clientes", headerRowIndex: 4, range: "C6:C7" }],
+   })
+   if (loading) {
+      return <div>Loading...</div>
+   }
 
-      <h1>Rodando do vscode projeto apps scricpt com react</h1>
-      
-    </>
+   if (error) {
+      return <div>Error!</div>
+   }
+   console.log("response =>", response)
+   const [{ data }] = response
 
-  );
+   return (
+      <>
+         <h1>Rodando do vscode projeto apps scricpt com react TESTE</h1>
+         <SelectSourch
+            titulo="Escolha um cliente"
+            initialData={data.map((c) => [c.CLIENTE])}
+         />
+      </>
+   )
 }
 
-export default Home;
+export default Home
