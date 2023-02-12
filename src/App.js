a2d11fe9e48829a03c82fe06613a3794fb2a63f4
claudiomf1 <claudiomf1+1@gmail.com>
2023-02-12 14:10:17 -0300

    cadastro-geral iniciado

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import React from "react"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
 
-import  React  from 'react'
-import Routes  from './route.js'
+import { Link } from "react-router-dom"
+import AppRoutes from "./route.js"
 
 function App() {
- return (
-
-     <>
-       <Routes />
-    </>
- 
- )
+   return (
+      <div>
+         <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="/">Meu Projeto</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+               <Nav className="mr-auto">
+                  <Nav.Link as={Link} to="/">
+                     Home
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/cadastro-geral">
+                     Cadastro Geral
+                  </Nav.Link>
+               </Nav>
+            </Navbar.Collapse>
+         </Navbar>
+         <AppRoutes />
+      </div>
+   )
 }
 
 export default App
